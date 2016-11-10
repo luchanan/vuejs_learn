6bbf3e997ded48fba5e8f6a6d4de1facfb783b1f
“yluchanan <398778994@qq.com>
2016-11-10 18:35:14 +0800

    home

---
+++ src/components/home/home-header.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>{{ msg }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: '头部'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
