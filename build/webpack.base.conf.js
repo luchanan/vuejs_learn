29b0851e68c5b361d6b20e6bf5806498f754d627
“luchanan <398778994@qq.com>
2016-11-11 00:25:02 +0800

    import css

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -19,7 +19,8 @@ module.exports = {
       'vue$': 'vue/dist/vue',
       'src': path.resolve(__dirname, '../src'),
       'assets': path.resolve(__dirname, '../src/assets'),
-      'components': path.resolve(__dirname, '../src/components')
+      'components': path.resolve(__dirname, '../src/components'),
+      'flexible': path.resolve(__dirname, '../src/assets/js/lib/flexible')
     }
   },
   resolveLoader: {
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App'
 import routers from './routers'
+// import './assets/js/lib/flexible.js'
+// 引入css
+import './assets/css/test.css'
 // 路由器会创建一个 App 实例，并且挂载到选择符 #app 匹配的元素上。
 new Vue({
   router: routers,
